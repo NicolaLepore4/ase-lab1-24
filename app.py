cb28d376f46b5545a7ab987ac4c77e02ebcec59c
NicolaLepore4 <n.lepore@studenti.unipi.it>
2024-09-27 12:09:08 +0200

    temp

--- app.py
+++ app.py
@@ -59,6 +59,22 @@ def random():
     else:
         return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
     
-    
+
+# /upperwhichgiven the string a it returns it in a JSON all in uppercase.
+# /lowerwhichgiven the string a it returns it in a JSON all in lowercase.
+# /concatwhichgiven the strings a and b it returns in a JSON the concatenation of them.
+# /reducewhichtakes the operator op (one of add, sub, mul, div, upper, lower, concat) and a lst string
+ representing a list and apply the operator to all the elements giving the result. For instance, 
+/reduce?op=add&lst=[2,1,3,4]returnsa JSON containing {s=10}, meaning 2+1+3+4.
+ ▪ /crashwhichterminates the service execution after responding to the client with info about the host and 
+the port of the service.
+ ▪ /lastwhichreturnsa string representing the last operation requested with success, in the format 
+op(args)=res, e.g. add(2.0,3.0)=5.0 or reduce(‘add’,[2,1,3,4])=10 or rand(1,3)=2. It answers with 
+HTTP code 404 if no operation was performed.
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
